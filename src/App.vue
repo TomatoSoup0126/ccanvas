b1f4fbc79af983df9d2379c6a44bd0e1d3ecb8fe
TomatoSoup <jonathantang0126@gmail.com>
2022-03-09 03:15:28 +0800

    add axios and fetch youtube and ig items

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,46 @@
 <script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import instagramItemCardVue from './components/instagramItemCard.vue';
+import axios from "axios"
+import { onMounted, ref } from "vue"
+
+let youtubeItems = ref([])
+let instagramItems = ref([])
+
+async function fetchPlayListItems() {
+  const { data } = await axios({
+    url: `${import.meta.env.VITE_YOUTUBE_API_BASE}playlistItems`,
+    params: {
+      key: import.meta.env.VITE_YOUTUBE_KEY,
+      playlistId: import.meta.env.VITE_PLAYLIST_ID,
+      part: 'snippet,contentDetails',
+      maxResults: 50
+    }
+  })
+  youtubeItems.value = data.items
+}
+
+async function fetchInstagramData() {
+  const { data } = await axios({
+    url: 'https://graph.instagram.com/me/media?&access_token',
+    params: {
+      fields:'caption,id,media_type,media_url,timestamp',
+      access_token: import.meta.env.VITE_IG_TOKEN
+    }
+  })
+  instagramItems.value = data.data
+  instagramItems.value.splice(instagramItems.value.length - 1, 1)
+}
+
+onMounted(()=>{
+  // fetchPlayListItems()
+  fetchInstagramData()
+})
+
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <!-- <img v-for="item in youtubeItems" :key="item.etag" :src="item.snippet.thumbnails.high.url" alt=""> -->
+  <instagramItemCardVue v-for="item in instagramItems" :key="item.id" :item="item" />
 </template>
 
 <style>
---
+++ src/components/instagramItemCard.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { ref, computed } from 'vue';
+
+const props = defineProps<{ item: object }>()
+
+const title = computed(() => {
+  return props.item.caption ? props.item.caption.split('\n\n')[0] : ''
+})
+
+const youtubeLink = computed(() => {
+  return props.item.caption ? props.item.caption.split('\n\n')[1]?.split(' ')[1] : ''
+})
+
+
+</script>
+
+<template>
+  <div>
+    <img :src="item.media_url" class="image" />
+    <h1>{{ title }}</h1>
+    <a :href="youtubeLink" target="_blank">{{ youtubeLink }}</a>
+  </div>
+</template>
+
+<style scoped>
+.image {
+  width: 500px;
+}
+</style>
