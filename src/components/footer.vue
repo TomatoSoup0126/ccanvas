b1c2dd88216e77b20d1360e9fb85f2b1bbd26ef5
TomatoSoup <jonathantang0126@gmail.com>
2022-03-26 00:05:48 +0800

    adjust footer and ig card height

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -1,5 +1,5 @@
 <template>
-  <footer class="absolute bottom-0 left-0 w-full">
+  <footer class="w-full">
     <div class="flex-center">
       <p class="text-xs text-align-center font-color-light">Copyright © C.CANVAS, All Rights Reserved.</p>
     </div>
